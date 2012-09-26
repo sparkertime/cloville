db514f185c4209da85b5152e53426cf27ccff45a
sparker <scott.p.parker@gmail.com>
2012-09-26 08:45:19 -0500

    Drawing a road

--- src/cloville/core.clj
+++ src/cloville/core.clj
@@ -11,16 +11,16 @@
   []
   (background 255)
   (dorun
-   (let [x-start 5
-         y-start 5]
-     (for [y (range-incl (height))
-           x (range-incl (width))]
-       (let [x-noise (mul-add x 0.01 x-start)
-             y-noise (mul-add y 0.01 y-start)
-             alph    (* 255 (noise x-noise y-noise))]
-         (stroke-weight 1)
-         (stroke-int 0 alph)
-         (line x y (inc x) (inc y))))))
+    (let [x-start 5
+          y-start 5]
+      (for [y (range-incl (height))
+            x (range-incl (width))]
+        (let [x-noise (mul-add x 0.01 x-start)
+              y-noise (mul-add y 0.01 y-start)
+              alph    (* 255 (noise x-noise y-noise))]
+          (stroke-weight 1)
+          (stroke-int 0 alph)
+          (line x y (inc x) (inc y))))))
   @heightmap)
 
 (defn setup
@@ -82,59 +82,87 @@
   (bit-and rgba 255))
 
 (defn population-at
-  "Given an x-y pair, returns the population at that point. Since the image is grayscale, this is simply any of the rgb values"
-  [x y]
-  (red (nth @heightmap (+ x (* y size-x)))))
+  "Given a point, returns the population at that point. Since the image is grayscale, this is simply any of the rgb values"
+  [[x y]]
+  (if (and (< -1 x size-x) (< -1 y size-y))
+    (red (nth @heightmap (+ x (* y size-x))))
+    0))
 
 ;
 ; The guts of road drawing AKA the interesting, gnarly bits
 ;
 
-(def length 10.0)
+(def road-length 10.0)
 
-(def dummy-road {:x 321 :y 420 :angle 91})
+(def dummy-road {:point [321 420] :angle 91})
 
 (defn endpoint-for
-  ([{:keys [x y angle]}] (endpoint-for x y angle))
-  ([{:keys [x y angle]} length] (endpoint-for x y angle length))
-  ([x y angle] (endpoint-for x y angle length))
-  ([x y angle length]
-     [(round (+ x (* length (sin (radians angle)))))
-      (round (+ y (* length (cos (radians angle)))))]))
+  ([{:keys [point angle]}] (endpoint-for point angle road-length))
+  ([[x y] angle length]
+   [(round (+ x (* length (sin (radians angle)))))
+    (round (+ y (* length (cos (radians angle)))))]))
 
 (def roads (atom []))
 
 (defn local-constraints [road]
   "Checks the local constraints for a road and returns a new road that satisfies the local constraints, or null if none can be found. These local constraints include...
-    * Checking roadway against water boundaries and truncating it up to a certain factor
-    * Checking for intersections within a radius of the end segment and adjusting appropriately"
+  * Checking roadway against water boundaries and truncating it up to a certain factor
+  * Checking for intersections within a radius of the end segment and adjusting appropriately"
   [])
 
 (defn new-roads []
   "Implementation of Global Contraints. Adds branches with delays in accordance to our global rules (attract to populations, follow global-rules (NY, Paris, SF, 'Basic'). Not entirely sure what this does yet" [])
 
+(defn offsets-to-check
+  "Returns all angle/length pairs to check when building a new segment"
+  []
+  (for [angle (map #(+ (* 45 %) (round (random -5 5))) (range 8))
+        length (map #(* (inc %) road-length) (range 9))]
+    {:angle angle :length length}))
+
+(defn most-populous-offset
+  "Given a point, returns the angle/length pair towards the most populous destination"
+  [p]
+  (reduce (fn [offset1 offset2]
+            (if (< (population-at (endpoint-for p (:angle offset1) (:length offset1)))
+                   (population-at (endpoint-for p (:angle offset2) (:length offset2))))
+              offset2
+              offset1)) (offsets-to-check)))
+
+(defn next-generation
+  "Given a set of roads, returns the next generation of roads available"
+  [roads]
+  (if (empty? roads)
+    (let [point (take 2 (repeatedly #(round (random 0 size-x))))]
+      [{:point point :angle (:angle (most-populous-offset point))}])
+    roads))
+
 (defn mouse-pressed
   "Upon a mouse press, outputs the population at that coordinate. Used for inspection"
   []
-  (let [x (mouse-x)
-        y (mouse-y)]
-  (println (str "Population at (" x "," y "): " (population-at x y)))))
+  (let [point [(mouse-x) (mouse-y)]]
+    (println (str "Population at (" point "): " (population-at point)))
+    (println (str "Most populous angle:" (:angle (most-populous-offset point))))))
 
 (defn draw-roads
   "Draws all roads for the current generation"
-  []
+  [all-roads]
   (stroke 255 0 0 255)
   (stroke-weight 2)
   (dorun
-    (for [road @roads]
-      (apply line road))))
+    (for [road all-roads]
+      (let [p1 (:point road)
+            p2 (endpoint-for road)]
+        (apply line (flatten [p1 p2]))))))
 
 (defn key-pressed
-  "Advances the state of the simulation by one generation if the up arrow is pressed"
+  "When the up arrow is pressed, advance the sim and redraw"
   []
   (if (= (key-as-keyword) :up)
-    (draw-background)
-    (draw-roads)))
+    (do
+      (swap! roads next-generation)
+      (draw-background)
+      (draw-roads @roads))))
 
 (defn launch
   "Starts the simulation"
